ee53f24b8a01775b76b63cb3b689fdd9317ffb1a
pramod-git-hub <vpramodkumar598@gmail.com>
2025-12-01 00:27:35 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import '../styles/globals.css'
+import { SessionContextProvider } from '@supabase/auth-helpers-react'
+import { supabase } from '../lib/supabaseClient'
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  // Prevent crash during prerender (404, 500, static build)
+  if (!supabase) return <Component {...pageProps} />
+
+  return (
+    <SessionContextProvider supabaseClient={supabase}>
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  )
 }
