7cce16488eef3c7e928b4352f2e3ccaa9dadc096
pramod-git-hub <vpramodkumar598@gmail.com>
2025-12-01 00:19:26 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,5 @@
 import '../styles/globals.css'
-import { SessionContextProvider } from '@supabase/auth-helpers-react'
-import { supabase } from '../lib/supabaseClient'
+
 export default function MyApp({ Component, pageProps }) {
-  return (
-    <SessionContextProvider supabaseClient={supabase}>
-      <Component {...pageProps} />
-    </SessionContextProvider>
-  )
+  return <Component {...pageProps} />
 }
