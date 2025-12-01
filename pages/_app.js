04e6cacda4c3174dc34e65c0c086e3529745b628
pramod-git-hub <vpramodkumar598@gmail.com>
2025-12-01 22:51:35 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
 export const dynamic = 'force-dynamic'
+
 import '../styles/globals.css'
 import { SessionContextProvider } from '@supabase/auth-helpers-react'
 import { supabase } from '../lib/supabaseClient'
 
 export default function MyApp({ Component, pageProps }) {
+
+  // IMPORTANT FIX – prevents crash during build
+  if (!supabase) {
+    return <Component {...pageProps} />
+  }
+
   return (
     <SessionContextProvider supabaseClient={supabase}>
       <Component {...pageProps} />
